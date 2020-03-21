aec45edb6f36f44e5c35d70a5fd8c9752e9eda6d
TanoueDaisuke <tano.dai.0711@icloud.com>
2020-03-21 20:58:46 +0900

    App.jsのimport Reactの部分を編集

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import axios from "axios"
